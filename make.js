920a1f7d7f182e060a700b74903ee98f06942a41
avolfman <avolfman@spotware.com>
2022-05-26 14:35:34 +0300

    add template script

---
+++ make.js
@@ -0,0 +1,30 @@
+import { promises, open } from 'fs';
+import path from 'path';
+import { exec } from 'child_process';
+
+const [, , challengeName] = process.argv;
+
+exec(`git checkout master && git pull && git checkout -b ${challengeName}`);
+
+function getNextIndex() {
+    return promises
+        .readdir('./challenges')
+        .then((files) => files.map((file) => Number(file.split('-')[0])))
+        .then((indices) => Math.max(...indices) + 1);
+}
+
+const nextIndex = await getNextIndex();
+
+const newChallenge = path.resolve(
+    './challenges',
+    `${nextIndex}-${challengeName}`
+);
+
+await promises.mkdir(newChallenge);
+await promises.mkdir(path.resolve(newChallenge, '__tests__'));
+
+await promises.open(path.resolve(newChallenge, `${challengeName}.ts`), 'w');
+await promises.open(
+    path.resolve(newChallenge, '__tests__', `${challengeName}.test.ts`),
+    'w'
+);
