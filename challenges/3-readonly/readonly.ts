a7de0797953507b8f94ff6ecda68a85261bc37e5
avolfman <avolfman@spotware.com>
2022-05-26 13:35:07 +0300

    readonly

---
+++ challenges/3-readonly/readonly.ts
@@ -0,0 +1,36 @@
+/*
+  https://github.com/type-challenges/type-challenges/blob/main/questions/00007-easy-readonly/README.md
+  
+  7 - Readonly
+  -------
+  by Anthony Fu (@antfu) #easy #built-in #readonly #object-keys
+
+  ### Question
+
+  Implement the built-in `Readonly<T>` generic without using it.
+
+  Constructs a type with all properties of T set to readonly, meaning the properties of the constructed type cannot be reassigned.
+
+  For example
+
+  ```ts
+  interface Todo {
+    title: string
+    description: string
+  }
+
+  const todo: MyReadonly<Todo> = {
+    title: "Hey",
+    description: "foobar"
+  }
+
+  todo.title = "Hello" // Error: cannot reassign a readonly property
+  todo.description = "barFoo" // Error: cannot reassign a readonly property
+  ```
+
+  > View on GitHub: https://tsch.js.org/7
+*/
+
+export type MyReadonly<T> = {
+    readonly [P in keyof T]: T[P];
+};
